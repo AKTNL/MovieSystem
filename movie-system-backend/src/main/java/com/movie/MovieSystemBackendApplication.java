eab579302917f6504c9092c47878e01900276faa
kevin <16294851+white_tree@user.noreply.gitee.com>
2025-12-29 16:53:16 +0800

    新建项目

---
+++ movie-system-backend/src/main/java/com/movie/MovieSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieSystemBackendApplication.class, args);
+    }
+
+}
