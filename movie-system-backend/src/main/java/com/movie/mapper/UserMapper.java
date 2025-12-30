de18f5166102ca5713eaabafb9baefe230ba30c8
kevin <16294851+white_tree@user.noreply.gitee.com>
2025-12-30 19:13:44 +0800

    [新增]后端跨域配置 (CORS)

--- movie-system-backend/src/main/java/com/movie/mapper/UserMapper.java
+++ movie-system-backend/src/main/java/com/movie/mapper/UserMapper.java
@@ -2,6 +2,8 @@ package com.movie.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.movie.entity.User;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
 }
