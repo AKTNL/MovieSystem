0754790ac3a4231833085b3edc756d7d8ecc0488
kevin <16294851+white_tree@user.noreply.gitee.com>
2026-01-11 09:26:16 +0800

    [修改]Auth.java

--- movie-system-backend/src/main/java/com/movie/common/Auth.java
+++ movie-system-backend/src/main/java/com/movie/common/Auth.java
@@ -8,9 +8,3 @@ import java.lang.annotation.*;
 public @interface Auth {
     String value() default "admin";
 }
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface Auth {
-    String value() default "admin";
-}
